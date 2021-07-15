0f87847eefb73a32c13aa1768898a3f38b75bd92
DanoR <zheung@vip.qq.com>
2021-07-15 18:02:25 +0800

    up to `4.5.0` restruct into ES Module add preset middleware `parseRaw` WebSocket interface available now

--- index.js
+++ index.js
@@ -1,58 +1,160 @@
-const OS = require('os');
+import Wocker from './lib/Wock.js';
+import initMounterFold from './lib/mounter/http-fold.js';
+import initMounterFace from './lib/mounter/http-face.js';
+import initMounterFaceWock from './lib/mounter/wock-face.js';
 
-module.exports = async function($) {
-	const { C: { paths = {}, folds = [], faces = [], mare = {}, wock = false } } = $;
 
-	// 文件上传
-	$.multer = require('@koa/multer')({ dest: paths.temp || OS.tmpdir() });
+/** 加载中间件 */
+async function initHTTPMares($) {
+	const { C: { mare }, logWarn } = $;
 
-	// 加载Wock
-	if(wock && wock.enabled) {
-		await require('./wock')($, wock);
-	}
+	const maresBefore = [];
+	const maresAfter = [];
 
-	const beforeMare = mare && mare.before ? mare.before : [];
-	const afterMare = mare && mare.after ? mare.after : [];
+	for(const initerMare_ of mare?.before ?? []) {
+		try {
+			let initerMare;
 
-	const before = [];
-	const after = [];
-
-	for(const func of beforeMare) {
-		before.push(await func($));
-	}
-	for(const func of afterMare) {
-		after.push(await func($));
-	}
-
-	mare.before = before;
-	mare.after = after;
-
-	// 挂载
-	const mounters = [
-		[
-			folds,
-			await require('./mount/fold')($),
-			null,
-		],
-		[
-			faces,
-			await require('./mount/face')($),
-			await require('./wock/face')($, wock),
-		]
-	];
-
-	for(const [routs, mount, mountWock] of mounters) {
-		if(mountWock) {
-			for(const rout of routs) {
-				if(rout.wock !== 'only') { await mount(rout); }
-
-				if(rout.wock) { await mountWock(rout); }
+			if(typeof initerMare_ == 'string') {
+				initerMare = (await import(`./lib/mare/${initerMare_}.js`)).default;
 			}
+
+			maresBefore.push(await initerMare($));
 		}
-		else {
-			for(const rout of routs) {
-				await mount(rout);
-			}
+		catch(error) {
+			logWarn('加载[接口前置中间件]', error);
 		}
 	}
-};
+	for(const initerMare_ of mare?.after ?? []) {
+		try {
+			let initerMare;
+
+			if(typeof initerMare_ == 'string') {
+				initerMare = (await import(`./lib/mare/${initerMare_}.js`)).default;
+			}
+
+			maresAfter.push(await initerMare($));
+		}
+		catch(error) {
+			logWarn('加载[接口后置中间件]', error);
+		}
+	}
+
+	return [maresBefore, maresAfter];
+}
+
+/** 加载Wock中间件 */
+async function initWockMares($) {
+	const { C: { wock }, logWarn } = $;
+
+	const mareskBefore = [];
+	const maresAfter = [];
+	const maresUpgrade = [];
+	const maresClose = [];
+
+	for(const initerMare_ of wock?.mare?.before ?? []) {
+		try {
+			let initerMare;
+
+			if(typeof initerMare_ == 'string') {
+				initerMare = (await import(`./lib/mare/${initerMare_}.js`)).default;
+			}
+
+			mareskBefore.push(await initerMare($));
+		}
+		catch(error) {
+			logWarn('加载[接口前置中间件]', error);
+		}
+	}
+	for(const initerMare_ of wock?.mare?.after ?? []) {
+		try {
+			let initerMare;
+
+			if(typeof initerMare_ == 'string') {
+				initerMare = (await import(`./lib/mare/${initerMare_}.js`)).default;
+			}
+
+			maresAfter.push(await initerMare($));
+		}
+		catch(error) {
+			logWarn('加载[接口后置中间件]', error);
+		}
+	}
+	for(const initerMare_ of wock?.mare?.upgrade ?? []) {
+		try {
+			let initerMare;
+
+			if(typeof initerMare_ == 'string') {
+				initerMare = (await import(`./lib/mare/${initerMare_}.js`)).default;
+			}
+
+			maresUpgrade.push(await initerMare($));
+		}
+		catch(error) {
+			logWarn('加载[接口后置中间件]', error);
+		}
+	}
+	for(const initerMare_ of wock?.mare?.close ?? []) {
+		try {
+			let initerMare;
+
+			if(typeof initerMare_ == 'string') {
+				initerMare = (await import(`./lib/mare/${initerMare_}.js`)).default;
+			}
+
+			maresClose.push(await initerMare($));
+		}
+		catch(error) {
+			logWarn('加载[接口后置中间件]', error);
+		}
+	}
+
+	return [mareskBefore, maresAfter, maresUpgrade, maresClose];
+}
+
+/**
+ * #### 服务器系统默认港湾（渴望）
+ * @version 4.5.0-2021.07.15.01
+ */
+async function DesireDefaultHarb($) {
+	const { C: { faces, folds, wock } } = $;
+
+	// 挂载文件资源
+	const mountFoldHTTP = await initMounterFold($);
+
+	for(const rout of folds) {
+		await mountFoldHTTP(rout);
+	}
+
+
+	// 是否启用Wock
+	const isWock = wock && !wock.disable;
+
+
+	// 挂载HTTP接口
+	const [maresHTTPBefore, maresHTTPAfter] = await initHTTPMares($);
+	const mountFaceHTTP = await initMounterFace($);
+
+	for(const rout of faces) {
+		if(!isWock || rout.wock !== 'only') {
+			await mountFaceHTTP(rout, maresHTTPBefore, maresHTTPAfter);
+		}
+	}
+
+
+	// 挂载Wock接口
+	if(isWock) {
+		const [maresWockBefore, maresWockAfter, maresWockUpgrade, maresWockClose] = await initWockMares($);
+
+		$.W = new Wocker($, maresWockUpgrade, maresWockClose);
+
+		const mountFaceWock = await initMounterFaceWock($);
+		for(const rout of faces) {
+			if(rout.wock) {
+				await mountFaceWock(rout, maresWockBefore, maresWockAfter);
+			}
+		}
+	}
+}
+
+export default DesireDefaultHarb;
